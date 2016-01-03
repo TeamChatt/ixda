a68c53186437d65300f5270179af7a1aae18c387
Matt Kaemmerer <mattk@spiceworks.com>
2016-01-03 12:48:51 -0600

    WIP - setup player view to match wireframes

--- player/main.js
+++ player/main.js
@@ -11,3 +11,27 @@ connection(receive => {
 
   return send;
 });
+
+
+//UI stuff
+const attack_button      = document.querySelector('[data-action="attack"]');
+const attack_menu        = document.querySelector('#attack-menu');
+const attack_back_button = document.querySelector('#attack-menu [data-action="back"]');
+
+attack_button.addEventListener('click', () => {
+  attack_menu.classList.toggle('is-hidden');
+});
+attack_back_button.addEventListener('click', () => {
+  attack_menu.classList.toggle('is-hidden');
+});
+
+const defend_button      = document.querySelector('[data-action="defend"]');
+const defend_menu        = document.querySelector('#defend-menu');
+const defend_back_button = document.querySelector('#defend-menu [data-action="back"]');
+
+defend_button.addEventListener('click', () => {
+  defend_menu.classList.toggle('is-hidden');
+});
+defend_back_button.addEventListener('click', () => {
+  defend_menu.classList.toggle('is-hidden');
+});

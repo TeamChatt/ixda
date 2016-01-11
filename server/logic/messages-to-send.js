c11e76baf471b8c81047687b134b8e0ad7710d4d
Matt Kaemmerer <mattk@spiceworks.com>
2016-01-11 13:04:17 -0600

    Send a message when monster is defeated

--- server/logic/messages-to-send.js
+++ server/logic/messages-to-send.js
@@ -6,10 +6,10 @@ const playerCount = require('./player-count');
 const monster     = require('./monster');
 
 module.exports = function messagesToSend(roles_joined, messages_in){
-  const prop_player_count   = playerCount(roles_joined, messages_in)
-    .map((count) => `There are ${count} players`);
-  const prop_monster_health = monster(roles_joined, messages_in)
-    .map((health) => `Monster has ${health} hit points`);
+  const prop_player_count     = playerCount(roles_joined, messages_in);
+  const prop_monster_health   = monster(roles_joined, messages_in);
+  const evst_monster_defeated = prop_monster_health
+    .filter((health) => health === 0);
 
   //Force subscription so this keeps the correct value even if nobody needs to know it.
   prop_monster_health  .onValue(() => {});
@@ -21,13 +21,23 @@ module.exports = function messagesToSend(roles_joined, messages_in){
   //messages_out:
   return {
     evst_st_send_to_players:
-      evst_st_ping,
+      Bacon.mergeAll(
+        evst_st_ping,
+        evst_monster_defeated
+          .map(() => `Monster was defeated`)
+      ),
 
     evst_st_send_to_presenters:
       Bacon.mergeAll(
         evst_st_ping,
-        prop_monster_health.toEventStream(),
-        prop_player_count.toEventStream()
+        prop_monster_health
+          .map((health) => `Monster has ${health} hit points`)
+          .toEventStream(),
+        evst_monster_defeated
+          .map(() => `Monster was defeated`),
+        prop_player_count
+          .map((count) => `There are ${count} players`)
+          .toEventStream()
       ),
 
     evst_st_send_to_gms:

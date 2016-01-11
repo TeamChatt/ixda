3fa27bd6337b018feab182ce56fadf3eb4588bd2
Timothy Vaughan <shadow11679@gmail.com>
2016-01-10 17:08:24 -0800

    setting entry point, fixing gulp call

--- deploy.cmd
+++ deploy.cmd
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Gulp
 IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
  pushd "%DEPLOYMENT_TARGET%"
- call :ExecuteCmd .\node_modules\.bin\gulp
+ call :ExecuteCmd .\node_modules\.bin\gulp build
  IF !ERRORLEVEL! NEQ 0 goto error
  popd
 )

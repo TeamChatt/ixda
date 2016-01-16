f9b3fb1543dffa50323b8f4402fc6b115b750d00
Matt Kaemmerer <mattk@spiceworks.com>
2016-01-16 00:07:31 -0600

    Show timer effect on buttons during cooldown

--- player/menu/behavior.js
+++ player/menu/behavior.js
@@ -3,6 +3,7 @@
 const menu_elements = require('./elements');
 const menuEvents    = require('./events');
 const menuState     = require('./state');
+const timerSweep    = require('../../lib/effects/timer-sweep');
 
 
 const menu_events = menuEvents(menu_elements);
@@ -21,4 +22,17 @@ menu_state.prop_defend_menu_shown
     menu_elements.defend_menu
       .classList
       .toggle('is-hidden', !is_shown);
+  });
+
+menu_state.prop_cooldown
+  .onValue((is_cooldown) => {
+    menu_elements.attack_button.disabled = is_cooldown;
+    menu_elements.defend_button.disabled = is_cooldown;
+  });
+menu_state.prop_cooldown
+  .changes()
+  .filter((is_cooldown) => is_cooldown)
+  .onValue(() => {
+    timerSweep(menu_elements.attack_button, 5000);
+    timerSweep(menu_elements.defend_button, 5000);
   });
--- player/menu/state.js
+++ player/menu/state.js
@@ -23,8 +23,20 @@ module.exports = (menu_events) => {
     )
     .toProperty(false);
 
+
+  const evst_button_clicked = Bacon.mergeAll(
+      menu_events.evst_attack_click,
+      menu_events.evst_defend_click
+    );
+  const prop_cooldown = Bacon.mergeAll(
+      evst_button_clicked.map(() => true),
+      evst_button_clicked.delay(5000).map(() => false)
+    )
+    .toProperty(false);
+
   return {
     prop_attack_menu_shown: prop_attack_menu_shown,
-    prop_defend_menu_shown: prop_defend_menu_shown
+    prop_defend_menu_shown: prop_defend_menu_shown,
+    prop_cooldown:          prop_cooldown
   };
 };

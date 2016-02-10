65f79ef5fabee619025b0034fc6342f64919e225
Matt Kaemmerer <mattk@spiceworks.com>
2016-02-10 17:04:24 -0600

    Use arrow notation for functions

--- player/dom.js
+++ player/dom.js
@@ -4,7 +4,7 @@ const dom          = require('../lib/dom');
 const menu         = require('./menu/view');
 const notification = require('./notification/view');
 
-module.exports = function(events, state){
+module.exports = (events, state) => {
   return dom
     .open('main', {'class': 'main'})
       .view(menu(events, state))
--- player/main.js
+++ player/main.js
@@ -7,30 +7,13 @@ const connection = require('../lib/connection');
 const view = require('./view');
 document.querySelector('body').appendChild(view().el);
 
-//DOM elements
-const wizard_button  = document.querySelector('[data-action="role-wizard"]');
-const fighter_button = document.querySelector('[data-action="role-fighter"]');
-const attack_button  = document.querySelector('[data-action="attack"]');
-
 //DOM events
-const evst_st_role   = Bacon.mergeAll(
-    Bacon.fromEvent(wizard_button, 'click')
-      .map(() => 'wizard'),
-    Bacon.fromEvent(fighter_button, 'click')
-      .map(() => 'fighter')
-  )
-  .map((role) => ({type: 'role', value: role}))
-  .take(1);
-
-const evst_st_attack = evst_st_role
-  .flatMap(() => Bacon.fromEvent(attack_button, 'click'))
-  .map(() => ({type: 'attack'}));
+const evst_st_role   = Bacon.once('fighter');
 
 //Connection
 connection(evst_st_receive => {
   const send = Bacon.mergeAll(
-      evst_st_role,
-      evst_st_attack
+      evst_st_role
     );
 
   evst_st_receive
